cd47cd7f25d77fb5db416e1c8e557cbe6eb9851b
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-23 15:30:50 +0100

    `Updated dashboard layout and navbar component styles`

--- src/component/dashboard/navbar/navbar.jsx
+++ src/component/dashboard/navbar/navbar.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+"use client";
+import styles from "../navbar/navbar.module.css";
+import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const pathName = usePathname();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathName.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search" className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <div className={styles.item}>
+            <MdPublic size={20} />
+          </div>
+          <div className={styles.item}>
+            <MdNotifications size={20} />
+          </div>
+          <div className={styles.item}>
+            <MdOutlineChat size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
