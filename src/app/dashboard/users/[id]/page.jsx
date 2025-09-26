7d9d54d9f8f11f17b993362bfe486701a8ea78a8
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-26 22:26:19 +0100

    `Added form to SingleUserPage component and updated styles in singleuserpage.module.css`

--- src/app/dashboard/users/[id]/page.jsx
+++ src/app/dashboard/users/[id]/page.jsx
@@ -6,32 +6,35 @@ export default function SingleUserPage() {
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
-        {/* <div className={styles.imgContainer}>
+        <div className={styles.imgContainer}>
           <Image src={NoAvatar} alt="no avatar" fill />
-        </div> */}
+        </div>
         John Doe
       </div>
       <div className={styles.formContainer}>
-        <label>Username</label>
-        <input type="text" name="username" placeholder="John Doe" />
-        <label>Email</label>
-        <input type="email" name="email" placeholder="JohnDoe@gmail.com" />
-        <label>Password</label>
-        <input type="password" name="password" placeholder="John Doe" />
-        <label>Phone</label>
-        <input type="phone" name="phone" placeholder="+234" />
-        <label>Address</label>
-        <textarea type="text" name="address" placeholder="Lagos" />
-        <label>Is Admin?</label>
-        <select name="isAdmin" id="isAdmin">
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
-        <label>Is Active?</label>
-        <select name="isActive" id="isActive">
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="John Doe" />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="JohnDoe@gmail.com" />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="John Doe" />
+          <label>Phone</label>
+          <input type="phone" name="phone" placeholder="+234" />
+          <label>Address</label>
+          <textarea type="text" name="address" placeholder="Lagos" />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <button>Update</button>
+        </form>
       </div>
     </div>
   );
