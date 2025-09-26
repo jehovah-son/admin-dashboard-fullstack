8f07a40380ed2caf2c5b2e045a0a0c4dcba70e6e
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-26 22:46:44 +0100

    `Added CSS styles to login page component`

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import styles from "../../component/login/login.module.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
