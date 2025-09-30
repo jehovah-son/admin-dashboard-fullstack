1e53f3e29093cdb6d6f1db79b5ab5986c249854b
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-30 05:12:00 +0100

    `Update ConnectToDB function to handle missing MONGO environment variable`

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -5,7 +5,7 @@ export const ConnectToDB = async () => {
 
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.MONGO || "");
     connection.isConnected = db.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
