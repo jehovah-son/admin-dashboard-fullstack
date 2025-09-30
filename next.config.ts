502916425e28b2bd44838b24c8269f2d8805a064
jehovah-son <chukwukasuccess0@gmail.com>
2025-09-30 05:39:14 +0100

    `Update next.config.ts to use remotePatterns for image hosting and add logging to ConnectToDB function in utils.js`

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["imgs.search.brave.com"],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'imgs.search.brave.com'
-    //   }
-    // ]
+    // domains: ["imgs.search.brave.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imgs.search.brave.com'
+      }
+    ]
   },
 };
 
--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -6,6 +6,7 @@ export const ConnectToDB = async () => {
   try {
     if (connection.isConnected) return;
     const db = await mongoose.connect(process.env.MONGO_URI);
+    console.log("🔍 MONGO_URI:", process.env.MONGO_URI);
     connection.isConnected = db.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
