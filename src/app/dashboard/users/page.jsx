aeeeac6f507f17e3d9ae3d7c5a96d9d43591ed2c
jehovah-son <chukwukasuccess0@gmail.com>
2025-10-01 18:29:05 +0100

    `Added Suspense component to wrap Pagination component`

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import NoAvatar from "../../../../public/noavatar.png";
 import Pagination from "../../../component/dashboard/pagination/pagination";
 import { fetchUser } from "../../lib/data";
-
+import { Suspense } from "react";
 export default async function UserPage({ searchParams }) {
   const q = searchParams?.q || "";
   //default is going to be 1 if there is no page in the url is going to be 1
@@ -70,7 +70,9 @@ export default async function UserPage({ searchParams }) {
           ))}
         </tbody>
       </table>
-      <Pagination count={count} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Pagination count={count} />
+      </Suspense>
     </div>
   );
 }
